83c514fe3cde96f430578a193274e64b239ac6b9
Khai Nguyen <1192669+khaister@users.noreply.github.com>
2024-04-17 17:12:59 -0700

    Add initial tests

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,7 +1,64 @@
-import { dummy } from '../src';
+import { HospitalOrdering, Recipe, Meal, MealItem } from '../src';
 
-describe('dummy import test', () => {
+describe('ordering is intantiated', () => {
   it('is true', () => {
-    expect(dummy()).toBeTruthy();
+    const ordering = new HospitalOrdering(null);
+    expect(ordering).toBeTruthy();
+    expect(ordering.recipes).toBeNull();
+    expect(ordering.meals).toBeNull();
+  });
+});
+
+describe('ordering is intantiated', () => {
+  it('is true', () => {
+    const ordering = new HospitalOrdering(null);
+    expect(ordering).toBeTruthy();
+    expect(ordering.recipes).toBeNull();
+    expect(ordering.meals).toBeNull();
+  });
+});
+
+describe('ordering is intantiated with a recipe', () => {
+  it('is true', () => {
+    // arrange
+    const recipes = new Map();
+    const name = "apple pie slice";
+    const recipe = new Recipe(name, 10, 250, 150);
+    recipes.set(name, recipe);
+
+    // act
+    const ordering = new HospitalOrdering(recipes);
+
+    // assert
+    expect(ordering).toBeTruthy();
+    expect(ordering.recipes).toEqual(recipes);
+    expect(ordering.meals).toBeNull();
+  });
+});
+
+describe('ordering is intantiated with a recipe', () => {
+  it('is true', () => {
+    // arrange
+    const name = "apple pie slice";
+    const recipe = new Recipe(name, 10, 250, 150);
+    const recipes = new Map([[name, recipe]]);
+
+    // act
+    const ordering = new HospitalOrdering(recipes);
+    const meal = new Meal([new MealItem(recipe, 2)]);
+    ordering.createMeal(meal);
+
+    // assert
+    expect(ordering.meals?.size).toEqual(1);
+
+    const createdMeal = ordering.meals?.get(1);
+    expect(createdMeal?.id).toEqual(1);
+    expect(createdMeal?.items.length).toEqual(1);
+
+    const mealItem = createdMeal?.items[0];
+    expect(mealItem?.recipe).toEqual(meal.items[0].recipe);
+    expect(mealItem?.quantity).toEqual(meal.items[0].quantity);
+
+    expect(createdMeal?.totalFatContent()).toEqual(20);
   });
 });
